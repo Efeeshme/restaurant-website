e39786d8fc78c7519d57ad27b1a22e6491907999
Efe Esme <efeesme12@gmail.com>
2026-02-25 10:10:29 +0400

    Add /healthz endpoint and adjust settings for Koyeb health checks

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def healthz(_request):
+    return HttpResponse("ok", content_type="text/plain", status=200)
 
 urlpatterns = [
+    path("healthz", healthz),          # <-- Koyeb health check buraya
     path("admin/", admin.site.urls),
-    path("", include("web.urls")),
+    path("", include("web.urls")),     # senin mevcut site route’ların
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
